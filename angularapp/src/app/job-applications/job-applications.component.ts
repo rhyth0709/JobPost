c473a3a717396532bd124f8cc17974f21bfc5d4b
Iamneo <techadmin@iamneo.ai>
2023-11-26 10:13:27 +0000

    Backup Commit

--- angularapp/src/app/job-applications/job-applications.component.ts
+++ angularapp/src/app/job-applications/job-applications.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { JobPosition } from 'src/models/job-position.model';
+import { JobService } from '../services/job.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-job-applications',
   templateUrl: './job-applications.component.html',
@@ -7,13 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobApplicationsComponent implements OnInit {
 
-  constructor() { }
+  positions : JobPosition[]=[]
+
+  constructor(private ss :JobService,private fb:FormBuilder) { }
+
+  jobApplicationForm = this.fb.group({
+
+    jobPositionId : ['',Validators.required],
+    applicantName : ['',Validators.required]
+
+  })
 
   ngOnInit(): void {
+
+    this.ss.getPositionTitles().subscribe((data:JobPosition[])=>{
+      this.positions = data
+    })
+
   }
 
-  applyForJob(){
-    
+  applyForJob(formData : FormGroup){
+
+    if(formData.valid){
+      
+
+    }
+
   }
 
 }
